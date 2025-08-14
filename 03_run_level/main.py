ddf48e3c1768246d758a2776d6c62ca368dfa361
Muhammad Shahzad <160237343+shahzad006@users.noreply.github.com>
2025-08-14 14:16:15 +0500

    first comment

---
+++ 03_run_level/main.py
@@ -0,0 +1,31 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GOOGLE_API_KEY")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+
+result = Runner.run_sync(agent, "Hello, how are you.", run_config=config)
+
+print(result.final_output)
